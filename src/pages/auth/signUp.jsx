0db1482199baf78ace2787fccc9a30bc2e52ae4c
Ojo Oluwapelumi <oluwapelumiojo05@gmail.com>
2022-08-09 13:45:01 +0100

    Implemented the new design for the sign-up page. It is also now responsive.
    Thanks

--- src/pages/auth/signUp.jsx
+++ src/pages/auth/signUp.jsx
@@ -1,90 +1,92 @@
 import GoogleLogin from "react-google-login";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const SignUp = () => {
-  const handleGoogleLogin = (googleData) => {
-    console.log(googleData);
-  };
+    const handleGoogleLogin = (googleData) => {
+        console.log(googleData);
+    };
 
-  const handleGoogleLoginFailure = (result) => {
-    console.log(result);
-  };
+    const handleGoogleLoginFailure = (result) => {
+        console.log(result);
+    };
 
-  return (
-    <div>
-      <main className="sign-up-wrapper">
-        <div className="sign-up-icon">
-          <img src={require("../../assets/images/Logo.png")} alt="" className="thirtin-logo"/>
-          <img src={require("../../assets/images/favicon.png")} alt="" className="coloured-thirtin-logo"/>
-        </div>
-        <div className="sign-up-form">
-          <h3>Sign Up</h3>
-          <form className="w-100">
-            <div className="w-100 align-center">
-              <p>Full name</p>
-              <input
-                className="input-1"
-                type="text"
-                placeholder="Enter full name"
-              />
+    return (<div>
+        <main className="sign-up-wrapper">
+            <div className="left-flex">
+                <div className="sign-up-icon">
+                    <img src={
+                            require("../../assets/images/Logo.png")
+                        }
+                        alt=""
+                        className="thirtin-logo"/>
+                    <img src={
+                            require("../../assets/images/favicon.png")
+                        }
+                        alt=""
+                        className="coloured-thirtin-logo"/>
+                </div>
             </div>
-            <div className="w-100 align-center">
-              <p>Email</p>
-              <input
-                className="input-1"
-                type="email"
-                placeholder="Enter email address"
-              />
-            </div>
-            <div className="w-100 align-center">
-              <p>Password</p>
-              <div className="input">
-                <input type="password" placeholder="Enter Password" />
-                <img
-                  src={require("../../assets/images/view-password-icon.png")}
-                  alt=""
-                />
+            <div className="right-flex">
+                <div className="sign-up-form">
+                    <h3>Sign Up</h3>
+                    <form className="w-100">
+                        <div className="w-100 align-center">
+                            <p>Full name</p>
+                            <input className="input-1" type="text" placeholder="Enter full name"/>
+                        </div>
+                        <div className="w-100 align-center">
+                            <p>Email</p>
+                            <input className="input-1" type="email" placeholder="Enter email address"/>
+                        </div>
+                        <div className="w-100 align-center">
+                            <p>Password</p>
+                            <div className="input">
+                                <input type="password" placeholder="Enter Password"/>
+                                <img src={
+                                        require("../../assets/images/view-password-icon.png")
+                                    }
+                                    alt=""/>
+                            </div>
+                            <h1 className="password-requirements">Password must contain upper case, Lower case, number and a special character</h1>
+                        </div>
+                
+                        <div className="w-100 align-center">
+                            <p>Confirm Password</p>
+                            <div className="input">
+                                <input type="password" placeholder="Enter Password"/>
+                                <img src={
+                                        require("../../assets/images/view-password-icon.png")
+                                    }
+                                    alt=""/>
+                            </div>
+                        </div>
+                        <button type="submit">Sign Up</button>
+                        <h5>
+                            Already have an account?<Link to="/login">Login</Link>
+                        </h5>
+                    </form>
+                </div>
+                <h4>Or Sign up with</h4>
+                <div className="google-wrapper">
+                    <GoogleLogin className="google" buttonText="Login with Google"
+                        onSuccess={handleGoogleLogin}
+                        onFailure={handleGoogleLoginFailure}
+                        cookiePolicy={"single_host_origin"}/>
+                </div>
+                <div className="sign-up-method mgt-48">
+                    <div>
+                        <img src={
+                                require("../../assets/images/linkedin-icon.png")
+                            }
+                            alt=""/>
+                    </div>
+                    <div>
+                        <h6>Login with Linkedin</h6>
+                    </div>
+                </div>
               </div>
-            </div>
-            <div className="w-100 align-center">
-              <p>Confirm Password</p>
-              <div className="input">
-                <input type="password" placeholder="Enter Password" />
-                <img
-                  src={require("../../assets/images/view-password-icon.png")}
-                  alt=""
-                />
-              </div>
-            </div>
-            <button type="submit">Sign Up</button>
-            <h5>
-              Already have an account?<Link to="/login">Login</Link>
-            </h5>
-          </form>
+            </main>
         </div>
-        <h4>Or Sign up with</h4>
-        <div className="google-wrapper">
-          <GoogleLogin
-            className="google"
-            buttonText="Login with Google"
-            onSuccess={handleGoogleLogin}
-            onFailure={handleGoogleLoginFailure}
-            cookiePolicy={"single_host_origin"}
-          />
-        </div>
-        <div className="sign-up-method mgt-48">
-          <div>
-            <img
-              src={require("../../assets/images/linkedin-icon.png")}
-              alt=""
-            />
-          </div>
-          <div>
-            <h6>Login with Linkedin</h6>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
-};
-export default SignUp;
+        );
+                                };
+                                export default SignUp;
