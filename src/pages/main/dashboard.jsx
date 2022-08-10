af27d4fee27dbeb66187f2a55df1ddfd497ed3dc
Paul_Aroo <timmypaul2006@gmail.com>
2022-08-10 22:18:25 +0100

    implement: route to dashboard

--- src/pages/main/dashboard.jsx
+++ src/pages/main/dashboard.jsx
@@ -1,20 +1,45 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import DashboardBar from "../../components/dashboard-bar";
 import TemplateCard from "../../components/templateCard";
 
 const Dashboard = () => {
-    return(
-        <div className="dashboard">
-            <DashboardBar/>
-            <div className="dashboard-content">
-                <h1>Hi, Ayobami Israel</h1>
-                <p>Start building from here, choose from our customizable templates to create your website with just a click.</p>
-                <div className="dashboard-templates">
-                    <TemplateCard href="#" text="Profile Card Template" src={require("../../assets/images/profile-card.png")}/>
-                    <TemplateCard href="#" text="Portfolio Website Template" src={require("../../assets/images/portfolio.png")}/>
-                    <TemplateCard href="#" text="Business Website Template" src={require("../../assets/images/business-website.png")}/>
-                </div>
-            </div>
+  const location = useLocation();
+  const [name, setName] = useState("Ayobami Israel");
+
+  useEffect(() => {
+    console.log(location);
+    if (location.state) setName(location.state.userDetails.fullname);
+  }, []);
+
+  return (
+    <div className="dashboard">
+      <DashboardBar name={name} />
+      <div className="dashboard-content">
+        <h1>Hi, {name}</h1>
+        <p>
+          Start building from here, choose from our customizable templates to
+          create your website with just a click.
+        </p>
+        <div className="dashboard-templates">
+          <TemplateCard
+            href="#"
+            text="Profile Card Template"
+            src={require("../../assets/images/profile-card.png")}
+          />
+          <TemplateCard
+            href="#"
+            text="Portfolio Website Template"
+            src={require("../../assets/images/portfolio.png")}
+          />
+          <TemplateCard
+            href="#"
+            text="Business Website Template"
+            src={require("../../assets/images/business-website.png")}
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default Dashboard;
