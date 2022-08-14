726957df5ada1e7c1856059855db99d6f092f764
johnoris <johnorisanwo@gmail.com>
2022-08-15 00:33:40 +0100

    fixing netlify error

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -8,7 +8,10 @@ import Login from "../pages/auth/login";
 import PasswordResetCode from "../pages/auth/passwordresetcode";
 import LandingPage from "../pages/main/landingpage";
 import AccountVerify from "../pages/auth/accountverify";
-import VerifySuccess from "../pages/auth/verifysuccess";
+import VerifySuccess from "../pages/auth/verifysuccess";\
+import PortfolioTemplate from "../components/templates/portfoliotemplate";
+import PortfolioCard from "../components/templates/portfoliocard";
+import BusinessTemplate from "../components/templates/businesstemplate";
 import Template from "../pages/main/template";
 import Dashboard from "../pages/main/dashboard";
 import DesignPage from "../pages/main/designspage";
